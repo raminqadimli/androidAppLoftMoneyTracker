aa3561ac2fa4b679782afe873dcc494b0f8f778a
Andrew <andrewbortnichuk@gmail.com>
2015-04-24 22:47:22 +0300

    Added TransactionsList

--- moneytracker/src/main/java/com/my/moneytracker/MainActivity.java
+++ moneytracker/src/main/java/com/my/moneytracker/MainActivity.java
@@ -2,38 +2,37 @@ package com.my.moneytracker;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView listView;
+    private TransactionAdapter transactionAdapter;
+    List <Transactions> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List <Transactions> adapterData = getDataList();
+        transactionAdapter = new TransactionAdapter(this,adapterData);
+
+        listView = (ListView)findViewById(R.id.listview);
+        listView.setAdapter(transactionAdapter);
+
     }
 
+    private List<Transactions> getDataList(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        data.add(new Transactions("Telephone","2000"));
+        data.add(new Transactions("T-Shirts","3000"));
+        data.add(new Transactions("Jeans","1000"));
+
+        return data;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
